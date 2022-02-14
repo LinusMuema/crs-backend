7e72a374d5296cd8204165f1107f60193a7c5966
linusmuema <linus.m.muema@gmail.com>
2022-02-14 15:22:43 +0300

    - some minor fixes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const middleware = require('../utils/middleware');
 const controller = require('../controllers/auth.controller');
 
-router.post("/signup", controller.signUp)
+router.post('/signup', controller.signUp)
 
-router.post("/login", controller.login)
+router.post('/login', controller.login)
 
 module.exports = router;
